6225946b3eb32dcff1f1c2c557572902714635d6
Guizaoo <guilhermexxzz54@gmail.com>
2022-05-11 16:47:50 -0300

    study function

---
+++ functions.js
@@ -0,0 +1,22 @@
+// const sum = function(number1, number2) {
+//     total = number1 + number2
+//     return total 
+// }
+
+
+// let number1 = 34
+// let number2 = 25
+
+// console.log(`o numero 1 é ${number1}`)
+// console.log(`o numero 2 é ${number2}`)
+// console.log(`a soma é ${sum(number1, number2)}`)
+// console.log(total)
+
+
+
+
+
+
+
+
+// Funçao e um liquidificador 
