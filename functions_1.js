cbdede7a9e7ef67726a5d4df7fd640adab41ce04
Guizaoo <guilhermexxzz54@gmail.com>
2022-05-11 16:41:34 -0300

    criando funçao

---
+++ functions_1.js
@@ -0,0 +1,17 @@
+// criar um aplicativo de frases motivacionais 
+
+// declaration - declaraçao da função
+
+// function statement
+function createPhrases() {
+    console.log('estudar é muito bom')
+    console.log('Paciência e persistência') 
+    console.log('Rvisão é mãe do aprendizado')         
+}
+
+// executar a função
+// rodar, chamar, invocar
+// execute, run, call, ivoke
+createPhrases()
+
+console.log('fim do programa')
