f25342be1af5b7c11b322ec7a84427e7a8681c59
Guizaoo <guilhermexxzz54@gmail.com>
2022-05-11 22:43:15 -0300

    study Manipulating data

---
+++ Prototype/scripts.js
@@ -0,0 +1,119 @@
+/*
+
+    Prototype
+
+    * prototype-based language
+    * prototype chain
+    * __proto__
+     
+*/
+
+/*
+
+    Type conversion (typecasting) vs typ coersin
+
+    *Alteração de um tipo de dado para outro tipo
+
+    console.log(Number('9') + 5)
+    console.log('9' + 5)
+
+*/
+
+/*
+
+Manipulando Strings e Números
+
+Transforma string em Número e Número em String 
+
+let string = "123"
+console.log(Number(string))
+
+let number = 321
+console.log(String(number))
+
+*/
+
+/*
+// Manipulando Strings e Números
+
+
+// Contar quantos caracteres tem uma palavara e quantos dígitos tem um número 
+
+let word = "Paralelepipedo"
+console.log(word.length)
+let number = 1234
+console.log(String(number).length)
+*/
+
+/*
+
+trasformando um número quebrado em 2 casas decimais e trocar ponto por vírgula 
+let number = 121.55
+console.log(number.toFixed(2))
+
+*/
+
+/*
+
+Transformando lentras minusculas em maiuscilas. Faça o contrario disso também
+
+let = word = "progrmar é muito bacana!"
+console.log(word.toLowerCase())
+console.log(word.toUpperCase())
+
+*/
+
+/*
+
+ Manipulando Strings
+
+ Verificar se o texto contem a palavra amor 
+
+ let phrase = "Eu quero viver o Amor!"
+ console.log(phrase.includes("Amor"))
+
+*/
+
+
+/*
+
+let phrase = "Eu quero viver o Amor!"
+let myArray = phrase.split(" ")
+let phraseWithUnderscore = myArray.join("_")
+console.log(phraseWithUnderscore.toLocaleLowerCase())
+
+*/
+
+/*
+
+// Manipulando Arrays
+
+
+// Criar Array com construtor
+let myArray = new Array('a', 'b', 'c')
+console.log(myArray)
+
+*/
+
+
+/*
+
+// Contar Elementos de um Array
+// console.log(["a", "b", "c"])
+// .length para sempre mostra quantos elementos tem 
+
+*/
+
+
+/* 
+
+// Manipulando Arroys
+
+// Transformar uma cadeia de caqracteres em elementos de um array
+
+let word = "manipulação"
+console.log(Array.from(word))
+
+*/
+
+
