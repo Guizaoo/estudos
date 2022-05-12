8bde4c3cdf7f3c0a6a3296dd007af065779bbb98
Guizaoo <guilhermexxzz54@gmail.com>
2022-05-12 19:45:55 -0300

    Estrutura de repetiçao

---
+++ Estruturas de repetição/scripts.js
@@ -0,0 +1,22 @@
+// Estrutura de repetiçao
+//for
+
+// for(let i = 100; i > 0; i--){
+//   if(i === 50) {
+//     break;
+//   }
+
+//   console.log(i)
+// }
+
+// Estrutura de repetiçao
+// While
+
+// for..of
+
+let name = "guilherme"
+let names = ('jao', 'pedro', 'paulo')
+
+for(let char of name){
+  console.log(char)
+}
