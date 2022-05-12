7efa1cc508402a142644157da9af95ee1f729e78
Guizaoo <guilhermexxzz54@gmail.com>
2022-05-12 13:07:01 -0300

    Expressões e Operadores

---
+++ Expressões e Operadores/scripts.js
@@ -0,0 +1,89 @@
+/* Expressões e Operadores 
+
+  - Expressions
+  - Operators
+
+  - Binari / let number = 1/console.log(number + 1)
+
+  - Unary / let number = 1/console.log(++number) / console.log(typeof number)  
+
+  - Ternary / let number = 1 console.log(true ? 'alo' : 'nada') para econtra alo e console.log(false ? 'alo' : 'nada')
+
+*/
+
+/*
+
+  new
+
+  * left-hand-side expression
+
+  * criar um novo objeto
+
+  let name = new String('Guilherme')
+  name.surName = "Dias"
+  let age = new Number(19)
+  console.log(name.surName, age)
+
+  Formato de DATA
+
+  let date = new Date('2020-12-01')
+  console.log(date)
+
+*/
+
+
+  // Operadores Aritiméticos
+
+  // multiplicaçoes
+  // console.log(3 * 4)
+
+  //Divisão
+  //console.log(12 / 2)
+
+  //soma 
+  //console.log(10 + 90)
+
+  //subtraçaão 
+  //console.log(80 - 60)
+
+/*
+  resto da divisão 
+  let remainder
+  remainder = 11% 9 
+  console.log(remainder)
+*/
+
+/*
+  //incremento ++
+
+  let incremento = 0 
+  incremento++
+  console.log(incremento)
+*/
+
+
+/*
+
+// decremento --
+
+let decremento = 0
+decremento--
+console.log(decremento)
+
+*/
+
+/*
+
+//exponecial **
+
+console.log(3 ** 3)
+
+*/
+
+
+
+
+
+
+
+
