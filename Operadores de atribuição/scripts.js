66b89271e5c52701efa5b15f771ede5fb155deda
Guizaoo <guilhermexxzz54@gmail.com>
2022-05-12 15:48:10 -0300

    Operadores de atribuiçao, Operadores lógicos, Operator precedence

---
+++ Operadores de atribuição/scripts.js
@@ -0,0 +1,116 @@
+// Operadores de atribuiçao (Assignment)
+let x 
+
+
+// assignment
+x = 1 
+
+// addition assignment 
+// x = x + 2
+x += 2
+
+// subtration assingment 
+// x = x - 1
+x -= 1
+
+// multiplication assingment
+//x = x 
+x*= 2
+
+// division assingment 
+// x = x * 2
+ x /=2
+ 
+// remainder, exponetiation 
+//x %= 2 
+//x **= 2
+//console.log(x)
+
+
+// Operadores lógicos (logical operators)
+
+// - 2 valores booleanos , quando verificados,
+// resultará em verdadeiro ou falso 
+
+// let pao = true 
+// let quijo = true
+
+
+// AND && /so se tiver os 2 
+// console.log(pao && quijo)
+
+//  OR || /um ou outro 
+// console.log(pao || quijo)
+
+// NOT ! /operador negaçao 
+// console.log(!pao)
+
+// let pao = true 
+// let quijo = true
+// const niceBreakfeast = pao && quijo ? 'café top': 'café ruim'
+// console.log(niceBreakfeast)
+
+// let pao = true 
+// let quijo = true
+// const niceBreakfeast = pao || quijo ? 'café top': 'café ruim'
+// console.log(niceBreakfeast)
+
+// let age = 20
+// const canDrive = age >= 18 ? 'can drive' : "can't drive"
+// console.log(canDrive)
+
+
+/*
+  FALSY
+  Qaundo um valor é considerado false em contextos onde um booleano é obrigatorio(condicionais e loops)
+
+  false
+  0
+  -0
+  ""
+  null
+  underfined
+  NaN
+
+*/
+// console.log("" ? 'verdadeiro' : 'falso')
+
+/*
+  TRUTHY 
+  Quando um valor é considerado true em contextos onde um booleano é obrigatório ( condicionais e loops )
+
+  true 
+  {}
+  []
+  1
+  3.23
+  "0"
+  "false"
+  -1
+  Infinity
+  -Infinity
+  console.log( {} ? 'verdadeiro' : 'falso')
+
+*/
+
+/* Operator precedence
+    Precedência de operadores
+
+  * grouping                         ( ) Ex: console.log( (5 + 2) * 10)
+  * negação e incremento              ! ++ -- 
+  * multiplicação e divisão           * /
+  * adição e subtração                + -
+  * relacional                        < <= > >=c
+  * igualdade                         == != === !==
+  * AND                               &&
+  * OR                                ||
+  * condicional                       ?:
+  * assignment (atribuição)           = += -= *=
+  
+  
+*/
+
+console.log()
+
+
+
