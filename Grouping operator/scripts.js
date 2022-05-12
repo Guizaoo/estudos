d96ce238e16f377bdefdb4c99994363f17166a65
Guizaoo <guilhermexxzz54@gmail.com>
2022-05-12 14:18:26 -0300

    Grouping operator

---
+++ Grouping operator/scripts.js
@@ -0,0 +1,46 @@
+/*
+
+// Grouping operator ( )
+let total = 2 + 5 * 3
+console.log(total)
+
+*/
+// Operadores de comparação 
+
+// Irá comparar valores e retornar um booolean como resposta à comparação
+let one = 1 
+let two = 2
+
+/*
+// == igual a 
+ console.log( two == 1 )
+ console.log( one == "1")
+
+*/
+
+/*
+// !== diferente de 
+ console.log(one != two)
+ console.log(one != 1)
+ console.log(one !="1")
+
+*/
+
+
+// == estritamente igual a 
+console.log( one === "1")
+console.log( one === 1)
+
+
+
+// !== estritamente diferente de 
+console.log(two !== "2")
+console.log(two !== 2)
+
+
+
+
+
+
+
+
