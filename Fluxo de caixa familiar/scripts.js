1ee70805ffb4e3ba2ffbef24f81e5388cf0e2141
Guizaoo <guilhermexxzz54@gmail.com>
2022-05-12 21:43:06 -0300

    Criando um objeto que possuirá propriedades, ambas do tipo array

---
+++ Fluxo de caixa familiar/scripts.js
@@ -0,0 +1,45 @@
+/* Criando um objeto que possuirá propriedades, ambas do tipo array:
+  
+    * receitas: []
+    * despesas: []
+
+*/
+
+let family ={
+  incomes: [2500, 3200, 250.45, 360.45],
+  expenses: [320.34, 128.45, 176.87, 1450.00]
+}
+
+  function sum(array) {
+    let total = 0;
+    
+    for(let value of array){
+        total += value
+      }
+  
+      return total
+}
+
+
+
+function calculateBalance() {
+      const calculateIncomes = sum(family.incomes)
+      const calculateExpenses= sum(family.expenses)
+      
+      const total = calculateIncomes - calculateExpenses
+
+      const istOK = total >= 0 
+
+      let belanceText = "Negativo"
+
+      if(istOK){
+        belanceText = "Positivo"
+      }
+
+      console.log(`Seu saldo é ${belanceText}: ${total}`)
+}
+
+calculateBalance()
+
+
+
